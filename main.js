800aece6fe712e2fb2a8f1c77e275267acdea13d
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-05-01 17:28:28 +0200

    if condition

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ function handSelection() {
 hands.forEach(hand => 
     hand.addEventListener('click', handSelection)
 )
+
+function compSelection() {
+    const aiComp = hands[Math.floor(Math.random() * 3)].dataset.option
+    return aiComp;
+}
+
+function checkResult (player, ai){
+    if(player === ai){
+        return 'draw'
+    } else if ((player === 'papier' && ai === 'kamien') || (player === 'kamien' && ai === 'nozyczki') || (player === 'nozyczki' && ai === 'papier')){
+        return 'win'
+        
+    } else {
+        return 'loss' 
+    }
+}
+
+function startGame() {
+    if(!game.playerHand) {
+        return alert('wybierz reke')
+    }
+    game.aiHand = compSelection()
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+}
+
+document.querySelector('.start').addEventListener('click', startGame)
